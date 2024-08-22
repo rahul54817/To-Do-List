fd627a5fae4160975b5fef97669447196218e9a5
rahul54817 <138282159+rahul54817@users.noreply.github.com>
2024-08-22 19:21:47 +0530

    Update AddList.jsx
    Modified Features

--- src/Components/AddList.jsx
+++ src/Components/AddList.jsx
@@ -3,19 +3,21 @@ import React from "react";
 import { useState } from "react";
 
 const AddList = () => {
-  const [task, setTask] = useState();
+  let [task, setTask] = useState("");
 
   const [list, setList] = useState([]);
 
   function addTaks(event) {
     event.preventDefault();
+    console.log("Task : " + task);
+
     if (task != "") {
       const data = {
         msg: task,
-        id : Date.now()
+        id: Date.now(),
       };
 
-      console.log(data);
+      console.log("Data  : " + data.msg);
 
       setList((prevList) => [...prevList, data]);
       setTask("");
@@ -31,24 +33,29 @@ const AddList = () => {
       <div className="container">
         <h1>To-Do List</h1>
 
-        <div className="add-todo-form">
-          <input
-            id="new-task"
-            type="text"
-            placeholder="Add new todo"
-            onChange={(e) => setTask({ task: e.target.value })}
-          />
-          <button onClick={(event) => addTaks(event)}>Add</button>
+        <div className="">
+          <form onSubmit={addTaks} className=" add-todo-form">
+            <input
+              id="new-task"
+              type="text"
+              placeholder="Add new todo"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+            />
+            <button type="submit"
+          
+            >
+            Add</button>
+          </form>
         </div>
       </div>
+      s
       <div>
-        {list.map((item) => (
-          <div className="todo-item container">
-            <input
-              type="checkbox"
-            />
-            <span>{item.msg.task}</span>
-            <button onClick={() => deletTask(item.id)} >Delete</button>
+        {list.reverse().map((item, index) => (
+          <div className="todo-item container" style={{padding : "15px"}}>
+            <p>{index +  1 + " ."}</p>
+            <span style= {{marginLeft : "10px"}}>{item.msg}</span>
+            <button onClick={() => deletTask(item.id)}>Delete</button>
           </div>
         ))}
       </div>
